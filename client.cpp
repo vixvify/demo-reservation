fdd9a16a13b2f5a77d820ffc455d16b928a61fc2
😺 <asnaweeezor@gmail.com>
2026-08-25 17:52:48 +0700

    feat: Enhance client-server communication with resource management commands

--- client.cpp
+++ client.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
 #include <cstring>
 
 #include <sys/ipc.h>
@@ -11,13 +10,19 @@ using namespace std;
 struct Message {
     long mtype;
     int client_id;
+    int resource_id;
+    char command[20];
     char text[100];
 };
 
 int main(int argc, char* argv[]) {
 
     if (argc != 2) {
-        cout << "Usage: " << argv[0] << " <client_id>\n";
+        cout
+            << "Usage: "
+            << argv[0]
+            << " <client_id>\n";
+
         return 1;
     }
 
@@ -27,42 +32,47 @@ int main(int argc, char* argv[]) {
 
     int msgid = msgget(key, 0666);
 
-    Message msg;
+    while (true) {
 
-    msg.mtype = 1;
-    msg.client_id = client_id;
+        Message msg;
 
-    cout << "Enter message: ";
+        msg.mtype = 1;
+        msg.client_id = client_id;
 
-    string input;
-    getline(cin, input);
+        cout << "Client-" << client_id << "> ";
 
-    strncpy(
-        msg.text,
-        input.c_str(),
-        sizeof(msg.text) - 1
-    );
+        cin >> msg.command;
 
-    msg.text[sizeof(msg.text) - 1] = '\0';
+        if (
+            strcmp(msg.command, "STATUS") == 0 ||
+            strcmp(msg.command, "RESERVE") == 0 ||
+            strcmp(msg.command, "CANCEL") == 0
+        ) {
+            cin >> msg.resource_id;
+        } else {
+            msg.resource_id = 0;
+        }
 
-    msgsnd(
-        msgid,
-        &msg,
-        sizeof(msg) - sizeof(long),
-        0
-    );
+        msgsnd(
+            msgid,
+            &msg,
+            sizeof(msg) - sizeof(long),
+            0
+        );
 
-    long response_type = 100 + client_id;
+        msgrcv(
+            msgid,
+            &msg,
+            sizeof(msg) - sizeof(long),
+            100 + client_id,
+            0
+        );
 
-    msgrcv(
-        msgid,
-        &msg,
-        sizeof(msg) - sizeof(long),
-        response_type,
-        0
-    );
-
-    cout << "Server: " << msg.text << '\n';
+        cout
+            << "Server: "
+            << msg.text
+            << "\n";
+    }
 
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -4,11 +4,15 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+#define MAX_RESOURCES 20
+
 using namespace std;
 
 struct Message {
     long mtype;
     int client_id;
+    int resource_id;
+    char command[20];
     char text[100];
 };
 
@@ -19,6 +23,8 @@ int main() {
         key,
         IPC_CREAT | 0666
     );
+
+    int reservation[MAX_RESOURCES] = {0};
 
     cout << "Server started..." << endl;
 
@@ -33,11 +39,86 @@ int main() {
             0
         );
 
-        cout << "Client "
-             << msg.client_id
-             << " sent: "
-             << msg.text
-             << endl;
+        cout
+            << "Client " << msg.client_id
+            << ": "
+            << msg.command
+            << " "
+            << msg.resource_id
+            << "\n";
+
+        if (strcmp(msg.command, "STATUS") == 0) {
+
+            int index = msg.resource_id - 1;
+
+            if (reservation[index] == 0) {
+                strcpy(
+                    msg.text,
+                    "AVAILABLE"
+                );
+            } else {
+                snprintf(
+                    msg.text,
+                    sizeof(msg.text),
+                    "RESERVED by Client %d",
+                    reservation[index]
+                );
+            }
+        }
+
+        else if (strcmp(msg.command, "RESERVE") == 0) {
+
+            int index = msg.resource_id - 1;
+
+            if (reservation[index] == 0) {
+
+                reservation[index] = msg.client_id;
+
+                strcpy(
+                    msg.text,
+                    "RESERVE SUCCESS"
+                );
+
+            } else {
+
+                snprintf(
+                    msg.text,
+                    sizeof(msg.text),
+                    "FAILED: already reserved by Client %d",
+                    reservation[index]
+                );
+            }
+        }
+
+        else if (strcmp(msg.command, "CANCEL") == 0) {
+
+            int index = msg.resource_id - 1;
+
+            if (reservation[index] == msg.client_id) {
+
+                reservation[index] = 0;
+
+                strcpy(
+                    msg.text,
+                    "CANCEL SUCCESS"
+                );
+
+            } else {
+
+                strcpy(
+                    msg.text,
+                    "CANCEL FAILED"
+                );
+            }
+        }
+
+        else {
+
+            strcpy(
+                msg.text,
+                "Unknown command"
+            );
+        }
 
         msg.mtype = 100 + msg.client_id;
 
